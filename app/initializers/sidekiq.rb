e46bedd70fefb61783ef8acc5c939aaee70725aa
Nelly Simkova <sk8high3r@ya.ru>
2024-04-21 03:57:09 +0300

    Change redis url for prod

--- app/initializers/sidekiq.rb
+++ app/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'sidekiq/web'
 
 Sidekiq.configure_server do |config|
-  config.redis = { :url => "redis:localhost:6379" }
+  config.redis = { :url => ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => "redis:localhost:6379" }
+  config.redis = { :url => ENV['REDIS_URL'] }
 end
